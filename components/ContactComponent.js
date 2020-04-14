ea634c6cacb9fb60c64a838511f9544522db643d
Daniel Osborn <danielosborn91@gmail.com>
2020-04-14 12:56:32 -0400

    Add files via upload

---
+++ components/ContactComponent.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import { ScrollView, Text } from "react-native";
+import { Card, Button, Icon } from 'react-native-elements';
+import * as Animatable from 'react-native-animatable';
+import * as MailComposer from 'expo-mail-composer';
+
+
+export default class Contact extends Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	static navigationOptions = {
+		title: "Contact Us"
+	};
+
+
+	sendMail() {
+		MailComposer.composeAsync({
+			recipients: ['campsites@nucamp.co'],
+			subject: 'Inquiry',
+			body: 'To whom it may concern:'
+		})
+	}
+
+	render() {
+
+		return (
+			<ScrollView>
+				<Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+
+					<Card title='Contact Information' wrapperStyle={{ margin: 20 }}>
+						<Text>1 Nucamp Way</Text>
+						<Text>Seattle, Wa 98001</Text>
+						<Text>U.S.A</Text>
+						<Text>Phone: 1-206-555-1234</Text>
+						<Text>Email: campsites@nucamp.co</Text>
+					</Card>
+
+					<Button
+						title="Send Email"
+						buttonStyle={{ backgroundColor: '#5637DD', margin: 40 }}
+						icon={<Icon
+							name='envelope-o'
+							type='font-awesome'
+							color='#fff'
+							iconStyle={{ marginRight: 10 }}
+						/>}
+						onPress={() => this.sendMail()}
+					/>
+				</Animatable.View>
+			</ScrollView>
+		);
+	}
+}
